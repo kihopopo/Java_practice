c19609c7192488a4955c4bc21f34b259024f11f1
kihopopo <104831865+kihopopo@users.noreply.github.com>
2023-02-10 17:35:59 +0900

    [create]定数を学ぼう〜確認問題〜

---
+++ Chapter04.java
@@ -0,0 +1,10 @@
+public class Chapter04 {
+    public static void main(String[] args){
+        final int LUCKY_NUMBER = 7;
+        String str = "加藤　希保";
+        System.out.println(str);
+        str = "一期一会";
+        System.out.println(str);
+        System.out.println(LUCKY_NUMBER);
+    }
+}
